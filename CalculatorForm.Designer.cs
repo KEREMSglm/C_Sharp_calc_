02dce65762a47cc70b407d965f8c9642ff73b12d
KEREMSglm <keremsaglam@live.com>
2023-03-18 20:39:29 +0300

    333

--- CalculatorForm.Designer.cs
+++ CalculatorForm.Designer.cs
@@ -18,7 +18,7 @@
                 components.Dispose();
             }
             base.Dispose(disposing);
-            Console.WriteLine("Dispose Calisdi");
+            Console.WriteLine("Dispose Calisdi.");
         }
 
         #region Windows Form Designer generated code
